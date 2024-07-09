c77aeaf9c550ab7debe96d52e292b6e9cca8c29d
ouoxii <yuchuner317@gmail.com>
2024-07-09 13:57:07 +0800

    fix: collaborator invitation message

--- src/TeamRepo.jsx
+++ src/TeamRepo.jsx
@@ -161,10 +161,10 @@ const TeamRepo = ({ onClose }) => {
                         });
                         if (!repoResponse.ok) throw new Error(`Failed to add collaborator to repo: ${repo.repoName}`);
                     }));
-                    alert('Successfully added collaborators to all repos');
+                    alert('成功寄出協作邀請');
                 } catch (error) {
-                    console.error('Error adding collaborators:', error);
-                    alert('Error adding collaborators');
+                    console.error('寄出協作邀請失敗', error);
+                    alert('寄出協作邀請失敗');
                 }
             }
             const inviteRefreshResponse = await fetch(`http://localhost:8081/invitations/${username}`, {
